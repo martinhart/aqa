6064d6440af3d6996cdea1e3d49c99cc34d57fe5
MH <martin.hart@mac.com>
2016-10-04 15:46:35 +0100

    Improve test coverage.

--- test/aqa/variable/VariablesImplTest.java
+++ test/aqa/variable/VariablesImplTest.java
@@ -5,6 +5,7 @@ package aqa.variable;
 
 import aqa.InterpreterException;
 import aqa.value.NullValue;
+import java.util.List;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.Before;
@@ -49,5 +50,22 @@ public class VariablesImplTest {
         Variable v2 = new Variable("a");
         subject.set(v1);
         subject.set(v2);
+    }
+    
+    @Test
+    public void testSize() throws InterpreterException {
+        assertEquals(0, subject.size());
+        subject.set(new Variable("a"));
+        assertEquals(1, subject.size());
+    }
+    
+    @Test
+    public void testGetNames() throws InterpreterException {
+        List<String> names;
+        
+        subject.set(new Variable("a"));
+        names = subject.getNames();
+        assertEquals(1, names.size());
+        assertEquals("a", names.get(0));
     }
 }
